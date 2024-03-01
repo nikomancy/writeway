9965d736a21fd43ef520427533c73a3cfee98d85
Niko <39569270+nikomancy@users.noreply.github.com>
2024-03-01 10:49:05 -0700

    Refactor MarkdownEditor component and add suggestion feature

--- src/markdownEditor.tsx
+++ src/markdownEditor.tsx
@@ -2,22 +2,43 @@ import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import styles from './MarkdownEditor.module.css'
 
-const MarkdownEditor: React.FC = () => {
-  const [markdown, setMarkdown] = useState<string>('y u no write markdown?')
+
+function MarkdownEditor() {
+  const [input, setInput] = useState('');
+  const [suggestion, setSuggestion] = useState('');
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    setTimer(setTimeout(() => {
+      setSuggestion('Lorem ipsum dolor sit amet, consectetur adipiscing elit.');
+    }, 2000)); // 2 seconds delay
+  }, [input]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Tab') {
+      event.preventDefault();
+      setInput(suggestion);
+      setSuggestion('');
+    }
+  };
 
   return (
-    <div className={styles.editorContainer}>
+    <div onKeyDown={handleKeyDown}>
       <textarea
-        className={styles.textarea}
-        value={markdown}
-        onChange={(e) => setMarkdown(e.target.value)}
-        placeholder='Write some markdown'
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        style={{ width: '100%', height: '200px' }}
       />
-      <div className={styles.markdown}>
-        <ReactMarkdown>{markdown}</ReactMarkdown>
-      </div>
+      <ReactMarkdown className={suggestion ? 'greyed-out' : ''}>
+        {suggestion || input}
+      </ReactMarkdown>
     </div>
-  )
+  );
 }
+
+export default MarkdownEditor;
 
 export default MarkdownEditor
